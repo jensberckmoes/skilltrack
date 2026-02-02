39ee37865661622593e5aeef589742cca4325842
jensberckmoes <jensberckmoeswerk@gmail.com>
2026-02-02 08:54:31 +0100

    Extracted some helper methods and naming has changed a bit to be more precise

--- src/main/java/com/sopra_steria/jens_berckmoes/service/LoginService.java
+++ src/main/java/com/sopra_steria/jens_berckmoes/service/LoginService.java
@@ -8,23 +8,35 @@ import com.sopra_steria.jens_berckmoes.model.*;
 import com.sopra_steria.jens_berckmoes.repository.TokenRepository;
 import com.sopra_steria.jens_berckmoes.repository.UserRepository;
 
-import static java.time.LocalDateTime.now;
+import java.time.LocalDateTime;
 
 public record LoginService(UserRepository userRepository, TokenRepository tokenRepository) {
     public LoginResult login(final Username username, final TokenValue tokenValue) {
+        return loginAt(username, tokenValue, LocalDateTime.now());
+    }
+
+    private LoginResult loginAt(final Username username, final TokenValue tokenValue, final LocalDateTime now) {
         try {
             final User user = userRepository.get(username);
             final Token token = tokenRepository.get(tokenValue);
-            if (!token.belongsTo(user)) {
-                throw new TokenDoesNotBelongToUserException();
-            }
-            if (token.isExpiredAt(now())) {
-                throw new TokenHasExpiredException();
-            }
+            ensureTokenBelongsToUser(token, user);
+            ensureTokenNotExpired(token, now);
             return LoginResult.success();
         } catch (final UserNotFoundException | TokenNotFoundException | TokenDoesNotBelongToUserException |
                        TokenHasExpiredException e) {
-            return LoginResult.of(LoginStatus.BLOCKED, "please contact support");
+            return LoginResult.blocked();
+        }
+    }
+
+    private static void ensureTokenBelongsToUser(final Token token, final User user) {
+        if (!token.belongsTo(user)) {
+            throw new TokenDoesNotBelongToUserException();
+        }
+    }
+
+    private static void ensureTokenNotExpired(final Token token, final LocalDateTime now) {
+        if (token.isExpiredAt(now)) {
+            throw new TokenHasExpiredException();
         }
     }
 }
