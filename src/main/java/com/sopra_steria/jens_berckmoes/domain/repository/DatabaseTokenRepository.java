88916943e979c709f9db46f98312027f40160c08
jensberckmoes <jensberckmoeswerk@gmail.com>
2026-02-05 10:57:09 +0100

    TDD- DatabaseTokenRepository implementation using TDD :)

--- src/main/java/com/sopra_steria/jens_berckmoes/domain/repository/DatabaseTokenRepository.java
+++ src/main/java/com/sopra_steria/jens_berckmoes/domain/repository/DatabaseTokenRepository.java
@@ -2,14 +2,23 @@ package com.sopra_steria.jens_berckmoes.domain.repository;
 
 import com.sopra_steria.jens_berckmoes.domain.Token;
 import com.sopra_steria.jens_berckmoes.domain.exception.TokenNotFoundException;
+import com.sopra_steria.jens_berckmoes.infra.entity.TokenEntity;
+import com.sopra_steria.jens_berckmoes.infra.mapping.TokenMapper;
+import com.sopra_steria.jens_berckmoes.infra.repository.CrudTokenRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Profile("prod")
+@AllArgsConstructor
 public class DatabaseTokenRepository implements TokenRepository {
+    private final CrudTokenRepository crudTokenRepository;
+
     @Override
     public Token findByTokenValue(final String token) throws TokenNotFoundException {
-        return null;
+        final TokenEntity possibleTokenEntity = crudTokenRepository.findById(token)
+                .orElseThrow(TokenNotFoundException::new);
+        return TokenMapper.mapToDomain(possibleTokenEntity);
     }
 }
--- src/main/java/com/sopra_steria/jens_berckmoes/infra/repository/CrudTokenRepository.java
+++ src/main/java/com/sopra_steria/jens_berckmoes/infra/repository/CrudTokenRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface CrudTokenRepository extends CrudRepository<TokenEntity, String> {
+
 }
