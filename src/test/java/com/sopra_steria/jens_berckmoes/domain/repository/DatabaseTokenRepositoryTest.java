d38c6a1951bdffc80d329e4051654c3fe1749dff
jensberckmoes <jensberckmoeswerk@gmail.com>
2026-02-12 14:46:29 +0100

    DatabaseTokenRepositoryTest refactored

--- src/test/java/com/sopra_steria/jens_berckmoes/domain/repository/DatabaseTokenRepositoryTest.java
+++ src/test/java/com/sopra_steria/jens_berckmoes/domain/repository/DatabaseTokenRepositoryTest.java
@@ -1,20 +1,29 @@
 package com.sopra_steria.jens_berckmoes.domain.repository;
 
+import com.sopra_steria.jens_berckmoes.TestConstants;
 import com.sopra_steria.jens_berckmoes.bdd.fakes.StepResult;
 import com.sopra_steria.jens_berckmoes.domain.Token;
 import com.sopra_steria.jens_berckmoes.domain.exception.TokenNotFoundException;
-import com.sopra_steria.jens_berckmoes.domain.exception.TokenValueNullException;
+import com.sopra_steria.jens_berckmoes.domain.valueobject.TokenValue;
 import com.sopra_steria.jens_berckmoes.infra.entity.TokenEntity;
 import com.sopra_steria.jens_berckmoes.infra.repository.CrudTokenRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static com.sopra_steria.jens_berckmoes.TestConstants.TokenEntities.*;
-import static com.sopra_steria.jens_berckmoes.TestConstants.TokenValues.*;
+import static com.sopra_steria.jens_berckmoes.TestConstants.TokenValues.ALICE_TOKEN_VALUE;
+import static com.sopra_steria.jens_berckmoes.TestConstants.TokenValues.BOB_TOKEN_VALUE;
 import static com.sopra_steria.jens_berckmoes.TestConstants.Tokens.*;
 import static com.sopra_steria.jens_berckmoes.bdd.fakes.StepResult.callController;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -34,6 +43,7 @@ class DatabaseTokenRepositoryTest {
 
         assertThat(result.isSuccess()).isFalse();
         assertThat(result.exceptionMessage()).isEqualTo("TokenValue cannot be null");
+        verify(crudTokenRepository, times(0)).findById(any());
     }
 
     @Test
@@ -61,54 +71,35 @@ class DatabaseTokenRepositoryTest {
     }
 
     @Test
-    @DisplayName("should save token and return the saved token with correct fields")
-    void shouldSaveToken() {
-        when(crudTokenRepository.save(ALICE_TOKEN_ENTITY)).thenReturn(ALICE_TOKEN_ENTITY);
-
-        final Token savedToken = repository.save(ALICE_TOKEN);
-
-        assertTokenFieldsAreEqual(savedToken, ALICE_TOKEN);
-    }
-
-    @Test
     @DisplayName("should actually save to the database when saving a token")
     void shouldActuallySaveToTheDatabase() {
         when(crudTokenRepository.save(ALICE_TOKEN_ENTITY)).thenReturn(ALICE_TOKEN_ENTITY);
 
-        final Token savedToken = repository.save(ALICE_TOKEN);
+        final StepResult<Token> result = callController(() -> repository.save(ALICE_TOKEN));
 
-        assertTokenFieldsAreEqual(savedToken, ALICE_TOKEN);
+        assertTokenFieldsAreEqual(result.body(), ALICE_TOKEN);
         verify(crudTokenRepository, times(1)).save(ALICE_TOKEN_ENTITY);
     }
 
     @Test
     @DisplayName("should return empty set when saving an empty set of tokens")
     void shouldReturnEmptySetWhenSavingEmptySet() {
+        when(crudTokenRepository.saveAll(any())).thenReturn(Set.of());
+
         final Set<Token> saved = repository.saveAll(Set.of());
+
         Assertions.assertThat(saved).isEmpty();
+        verify(crudTokenRepository,times(1)).saveAll(any());
     }
 
     @Test
-    @DisplayName("should delete all tokens and throw TokenNotFoundException when trying to find deleted tokens")
-    void shouldDeleteAllTokens() {
-        when(crudTokenRepository.findById(ALICE_TOKEN.token())).thenReturn(Optional.of(ALICE_TOKEN_ENTITY));
-        when(crudTokenRepository.findById(BOB_TOKEN.token())).thenReturn(Optional.of(BOB_TOKEN_ENTITY));
-
-        assertThat(repository.findByTokenValue(ALICE_TOKEN_VALUE)).isNotNull();
-        assertThat(repository.findByTokenValue(BOB_TOKEN_VALUE)).isNotNull();
-
+    @DisplayName("should be able to delete all tokens and throw TokenNotFoundException when trying to find deleted tokens")
+    void shouldDeleteAllTokens2() {
         repository.deleteAll();
-
-        when(crudTokenRepository.findById(ALICE_TOKEN.token())).thenReturn(Optional.empty());
-        when(crudTokenRepository.findById(BOB_TOKEN.token())).thenReturn(Optional.empty());
-
-        assertThatThrownBy(() -> repository.findByTokenValue(ALICE_TOKEN_VALUE)).isInstanceOf(
-                TokenNotFoundException.class);
-        assertThatThrownBy(() -> repository.findByTokenValue(BOB_TOKEN_VALUE)).isInstanceOf(
-                TokenNotFoundException.class);
 
         verify(crudTokenRepository, times(1)).deleteAll();
     }
+
 
     @Test
     @DisplayName("should save all tokens and return the saved tokens with correct fields")
@@ -128,6 +119,7 @@ class DatabaseTokenRepositoryTest {
     void deleteAllIsIdempotent() {
         repository.deleteAll();
         repository.deleteAll();
+        verify(crudTokenRepository,times(2)).deleteAll();
     }
 
     private static void assertTokenFieldsAreEqual(final Token tokenOnDatabase, final Token validToken) {
